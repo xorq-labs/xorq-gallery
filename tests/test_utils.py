35a060fecaad5b4342d0a632254a5c4ea1d167fd
dlovell <dlovell@gmail.com>
2026-03-11 19:42:22 -0400

    wip

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,13 @@
 from xorq_gallery.sklearn.utils import (
     get_exprs_dict,
     load_exprs_json_cache,
-    update_exprs_json_cache,
 )
 
 
 def test_load_exprs_json_cache_matches_get_exprs_dict():
-    update_exprs_json_cache()
-    assert load_exprs_json_cache() == get_exprs_dict()
+    cached = load_exprs_json_cache()
+    calculated = get_exprs_dict()
+    left, right = (
+        set((k, tuple(v)) for k, v in el.items()) for el in (cached, calculated)
+    )
+    assert left == right, (left.difference(right), right.difference(left))
